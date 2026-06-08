784aa2ff8f55ad09d8a8d20d47e04259e4ec0da4
stalnoisip-beep <stalnoiship@gmail.com>
2026-06-08 11:09:05 +0500

    AddAvalonia

---
+++ MyAvaloniaAppl/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace MyAvaloniaAppl.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Avalonia!";
+}
---
+++ MyAvaloniaAppl/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace MyAvaloniaAppl.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
