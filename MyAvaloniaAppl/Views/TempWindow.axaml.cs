0959d2459fc319ff8bcce83de79a62b8c43f4af3
stalnoisip-beep <stalnoiship@gmail.com>
2026-06-10 10:56:06 +0500

    add publish files

---
+++ MyAvaloniaAppl/Views/TempWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace MyAvaloniaAppl.Views;
+
+public partial class TempWindow : Window
+{
+    public TempWindow()
+    {
+        InitializeComponent();
+    }
+}
