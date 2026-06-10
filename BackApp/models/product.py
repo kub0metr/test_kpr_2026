ff18c125dc2d8fb5301828ccbc9f43e3ca580688
konovalovta <timurka2303@gmail.com>
2026-06-10 14:44:42 +0500

    fix() упрощено взаимодействие: добавлены необязательные поля

--- BackApp/models/product.py
+++ BackApp/models/product.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from datetime import datetime
 import enum
 
@@ -17,15 +17,15 @@ class ProductCategory(enum.Enum):
 class Product(BaseModel):
     """Класс для отображения Product в json"""
 
-    id: int
+    id: int = 0
     name: str
-    category: ProductCategory
-    list_ingredients: str
+    category: ProductCategory = ProductCategory.BREAD
+    list_ingredients: str | None = None
     price: float
     cost: float
-    is_seasonable: bool
-    is_selling: bool
-    first_date: datetime
+    is_seasonable: bool = False
+    is_selling: bool = False
+    first_date: datetime = Field(default_factory=lambda: datetime.now())
 
     class Config:
         from_attributes = True
